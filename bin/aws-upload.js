374d8bc9f45450f38d869e9e8c565106d9124d88
mario7746 <m.kennedykavouras@gmail.com>
2017-03-06 15:08:38 -0500

    Refactor bin script into module
    Moved all of the code from bin/aws-upload that was respnsible for
    the s3 upload piece to it's own  file in the lib directory.
    We saw the need for using this code in more than once place
    so the lib/s3-upload module can now be used from more than one place
    in our app
    The bin/aws-upload script still works(and just implempents the lib one)

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -2,58 +2,13 @@
 
 require('dotenv').load();
 
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const fs = require('fs');
-
-const mime = require('mime');
-const path = require('path');
-const crypto = require('crypto');
+const s3Upload = require('../lib/s3-upload.js');
 
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
-let mimeType = mime.lookup(file.path);
-let ext = path.extname(file.path);
-let folder = (new Date()).toISOString().split('T')[0];
-
-let stream = fs.createReadStream(file.path);
-
-new Promise ((res, rej)=>{
-  crypto.randomBytes(16, (err, buf)=>{
-    if(err){
-      rej(err);
-    }
-    else {
-      console.log("buffer is ", buf);
-      console.log("buffer.toS is ", buf.toString('hex'));
-
-      res(buf.toString('hex'));
-    }
-  });
-}).then((filename)=>{
-  let params = {
-    ACL: 'public-read',
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: `${folder}/${filename}${ext}`,
-    Body: stream,
-    ContentType: mimeType
-  };
-
-  return new Promise((res, rej) =>{
-    s3.upload(params, function (err, data){
-      if(err){
-        console.log(err);
-        rej(err);
-      }
-      else {
-        console.log(data);
-        res(data);
-      }
-    });
-  });
-})
-.then(console.log)
-.catch(console.error);
+s3Upload(file)
+  .then(console.log)
+  .catch(console.error);
